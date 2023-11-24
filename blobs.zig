9f5c5c998154622b1a87e9498733274c404f1af7
Jonathan Marler <johnnymarler@gmail.com>
2023-11-23 23:52:55 -0700

    more blobs!

--- blobs.zig
+++ blobs.zig
@@ -10,13 +10,19 @@ const min_radius_pt = 500;
 fn XY(comptime T: type) type {
     return struct { x: T, y: T };
 }
-const global = struct {
-    pub var rand: std.rand.DefaultPrng = undefined;
-    pub var pos = XY(i32){ .x = 0, .y = 0 };
-    pub var blob_radius_pt: i32 = min_radius_pt;
+
+const Blob = struct {
+    pos_pt: XY(i32),
+    radius_pt: i32,
     // Angle is in radians in the range of [0,2PI) (includes 0 but not 2PI).
     // 0 is to the right, PI/2 is upward and so on.
-    pub var angle: f32 = 0;
+    angle: f32,
+};
+
+const global = struct {
+    pub var rand: std.rand.DefaultPrng = undefined;
+    pub var blobs: [20]Blob = undefined;
+    var ai_controls = [_]Control{ .none } ** (global.blobs.len - 1);
 };
 
 fn log(comptime fmt: []const u8, args: anytype) void {
@@ -54,11 +60,23 @@ const _2pi = 2 * std.math.pi;
 
 var points_buf: [5000]XY(i32) = undefined;
 
-fn getRandomCoord() i32 {
-    var buf: [2]u8 = undefined;
+// returns a random f32 in the range [0,1) (includes 0 but not 1)
+// it uses (byte_count*8) bits of granualarity
+fn getRandomScale(comptime byte_count: comptime_int) f32 {
+    const UInt = @Type(std.builtin.Type{
+        .Int = .{
+            .signedness = .unsigned,
+            .bits = byte_count * 8,
+        },
+    });
+    var buf: [byte_count]u8 = undefined;
     global.rand.fill(&buf);
-    const r = std.mem.readInt(u16, &buf, .Big);
-    const mult = @as(f32, @floatFromInt(r)) / @as(f32, 65536);
+    const r = std.mem.readInt(UInt, &buf, .Big);
+    return @as(f32, @floatFromInt(r)) / (@as(f32, 1.0) + @as(f32, std.math.maxInt(UInt)));
+}
+
+fn getRandomCoord() i32 {
+    const mult = getRandomScale(2);
     const slot: i32 = @intFromFloat(arena_half_size_pt_f32 * 2 * mult);
     return slot - arena_half_size_pt;
 }
@@ -72,11 +90,11 @@ fn getRandomPoint() XY(i32) {
 
 fn ptToPxX(points_per_pixel: i32, coord_x: i32) i32 {
     if (points_per_pixel <= 0) unreachable;
-    return 80 + @divTrunc((coord_x - global.pos.x), points_per_pixel);
+    return 80 + @divTrunc((coord_x - global.blobs[0].pos_pt.x), points_per_pixel);
 }
 fn ptToPxY(points_per_pixel: i32, coord_y: i32) i32 {
     if (points_per_pixel <= 0) unreachable;
-    return 80 + @divTrunc((coord_y - global.pos.y), points_per_pixel);
+    return 80 + @divTrunc((coord_y - global.blobs[0].pos_pt.y), points_per_pixel);
 }
 fn ptToPx(points_per_pixel: i32, pt: XY(i32)) XY(i32) {
     return XY(i32){
@@ -115,82 +133,131 @@ fn getFreqs(radius: f32) struct { start: u16, end: u16 } {
     };
 }
 
+fn updateAngle(blob: *Blob, control: Control) void {
+    switch (control) {
+        .none => {},
+        .dec => {
+            blob.angle -= angle_speed;
+            if (blob.angle < 0) {
+                blob.angle += _2pi;
+                std.debug.assert(blob.angle >= 0);
+            }
+        },
+        .inc => {
+            blob.angle += angle_speed;
+            if (blob.angle > _2pi) {
+                blob.angle -= _2pi;
+                std.debug.assert(blob.angle <= _2pi);
+            }
+        },
+    }
+}
+
 export fn start() void {
     global.rand = std.rand.DefaultPrng.init(0);
     for (&points_buf) |*pt| {
         pt.* = getRandomPoint();
     }
+    global.blobs[0] = .{
+        .pos_pt = .{ .x = 0, .y = 0 },
+        .radius_pt = min_radius_pt,
+        .angle = 0,
+    };
+    for (global.blobs[1..], 0..) |*other, i| {
+        other.* = .{
+            .pos_pt = getRandomPoint(),
+            .radius_pt = min_radius_pt,
+            .angle = _2pi * getRandomScale(2),
+        };
+        global.ai_controls[i] = .none;
+    }
 }
 
 export fn update() void {
-
-    switch (getControl(
+    updateAngle(&global.blobs[0], getControl(
         0 != (w4.GAMEPAD1.* & w4.BUTTON_LEFT),
         0 != (w4.GAMEPAD1.* & w4.BUTTON_RIGHT),
-    )) {
-        .none => {},
-        .dec => {
-            global.angle -= angle_speed;
-            if (global.angle < 0) {
-                global.angle += _2pi;
-                std.debug.assert(global.angle >= 0);
+    ));
+
+    for (global.blobs[1..], 0..) |*blob, i| {
+        const control_ref = &global.ai_controls[i];
+        {
+            var buf: [1]u8 = undefined;
+            global.rand.fill(&buf);
+            switch (control_ref.*) {
+                .none => switch (buf[0]) {
+                    0...39 => control_ref.* = .dec,
+                    40...79 => control_ref.* = .inc,
+                    80...255 => {},
+                },
+                .dec => switch (buf[0]) {
+                    0...39 => control_ref.* = .none,
+                    40...255 => {},
+                },
+                .inc => switch (buf[0]) {
+                    0...39 => control_ref.* = .none,
+                    40...255 => {},
+                },
             }
-        },
-        .inc => {
-            global.angle += angle_speed;
-            if (global.angle > _2pi) {
-                global.angle -= _2pi;
-                std.debug.assert(global.angle <= _2pi);
-            }
-        },
+        }
+        updateAngle(blob, control_ref.*);
     }
+
+    // custom size change control (for development)
     switch (getControl(
         0 != (w4.GAMEPAD1.* & w4.BUTTON_DOWN),
         0 != (w4.GAMEPAD1.* & w4.BUTTON_UP),
     )) {
         .none => {},
-        .dec => global.blob_radius_pt = @max(
-            global.blob_radius_pt - 10, 10
+        .dec => global.blobs[0].radius_pt = @max(
+            global.blobs[0].radius_pt - 10, 10
         ),
-        .inc => global.blob_radius_pt += 10,
+        .inc => global.blobs[0].radius_pt += 10,
     }
 
-    const sin = std.math.sin(global.angle);
-    const cos = std.math.cos(global.angle);
+    var sines: [global.blobs.len]f32 = undefined;
+    var cosines: [global.blobs.len]f32 = undefined;
 
-    {
+    // move blobs
+    for (&global.blobs, 0..) |*blob, i| {
         // TODO: get slower as you get bigger
-        const diff_x: i32 = @intFromFloat(@floor(speed_points * cos));
-        const diff_y: i32 = @intFromFloat(@floor(speed_points * sin));
-        const min: i32 = -arena_half_size_pt + global.blob_radius_pt;
-        const max: i32 =  arena_half_size_pt - global.blob_radius_pt;
-        global.pos = .{
-            .x = clamp(i32, global.pos.x + diff_x, min, max),
-            .y = clamp(i32, global.pos.y + diff_y, min, max),
+        sines[i] = std.math.sin(blob.angle);
+        cosines[i] = std.math.cos(blob.angle);
+
+        const diff_x: i32 = @intFromFloat(@floor(speed_points * cosines[i]));
+        const diff_y: i32 = @intFromFloat(@floor(speed_points * sines[i]));
+        const min: i32 = -arena_half_size_pt + blob.radius_pt;
+        const max: i32 =  arena_half_size_pt - blob.radius_pt;
+        blob.pos_pt = .{
+            .x = clamp(i32, blob.pos_pt.x + diff_x, min, max),
+            .y = clamp(i32, blob.pos_pt.y + diff_y, min, max),
         };
     }
 
-    for (&points_buf) |*pt| {
-        const diff_x: f32 = @floatFromInt(pt.x - global.pos.x);
-        const diff_y: f32 = @floatFromInt(pt.y - global.pos.y);
-        const dist = std.math.sqrt(diff_x * diff_x + diff_y * diff_y);
-        if (dist < 0) @panic("here"); // impossible right?
-        if (dist >= @as(f32, @floatFromInt(global.blob_radius_pt))) continue;
-        //log("eat point {}!", .{i});
-        const freqs = getFreqs(@floatFromInt(global.blob_radius_pt));
-        //log("tone {} to {}", .{freqs.start, freqs.send});
-        const freq_arg = @as(u32, freqs.end) << 16 | freqs.start;
-        w4.tone(freq_arg, 5, 20, 0);
-        {
-            pt.* = getRandomPoint();
-            global.blob_radius_pt += 10;
+    // TODO: this *might* need some optimization?
+    for (&global.blobs) |*blob| {
+        for (&points_buf) |*pt| {
+            const diff_x: f32 = @floatFromInt(pt.x - blob.pos_pt.x);
+            const diff_y: f32 = @floatFromInt(pt.y - blob.pos_pt.y);
+            const dist = std.math.sqrt(diff_x * diff_x + diff_y * diff_y);
+            if (dist < 0) @panic("here"); // impossible right?
+            if (dist >= @as(f32, @floatFromInt(blob.radius_pt))) continue;
+            //log("eat point {}!", .{i});
+            const freqs = getFreqs(@floatFromInt(blob.radius_pt));
+            //log("tone {} to {}", .{freqs.start, freqs.send});
+            const freq_arg = @as(u32, freqs.end) << 16 | freqs.start;
+            w4.tone(freq_arg, 5, 20, 0);
+            {
+                pt.* = getRandomPoint();
+                blob.radius_pt += 10;
+            }
         }
     }
 
 
     const desired_blob_radius_px = 10;
     const points_per_pixel: i32 = @divTrunc(
-        global.blob_radius_pt,
+        global.blobs[0].radius_pt,
         desired_blob_radius_px
     );
     drawBars(points_per_pixel, .x);
@@ -204,25 +271,28 @@ export fn update() void {
     }
 
     // draw player circle
-    {
-        const px = ptToPx(points_per_pixel, .{
-            .x = global.pos.x - global.blob_radius_pt,
-            .y = global.pos.y - global.blob_radius_pt,
-        });
-        w4.DRAW_COLORS.* = 0x03;
-        const size: i32 = @divTrunc(global.blob_radius_pt * 2, points_per_pixel);
-        //log("player {},{} size={}", .{px.x, px.y, size});
-        w4.oval(px.x, px.y, @intCast(size), @intCast(size));
-    }
-    // draw the direction dot
-    {
-        const radius_pt: f32 = @as(f32, @floatFromInt(global.blob_radius_pt));
-        const px = ptToPx(points_per_pixel, .{
-            .x = global.pos.x + @as(i32, @intFromFloat(radius_pt * cos)),
-            .y = global.pos.y + @as(i32, @intFromFloat(radius_pt * sin)),
-        });
-        w4.DRAW_COLORS.* = 0x41;
-        w4.oval(px.x - 2, px.y - 2, 4, 4);
+    for (&global.blobs, 0..) |*blob, i| {
+        {
+            const px = ptToPx(points_per_pixel, .{
+                .x = blob.pos_pt.x - blob.radius_pt,
+                .y = blob.pos_pt.y - blob.radius_pt,
+            });
+            w4.DRAW_COLORS.* = 0x03;
+            const size: i32 = @divTrunc(blob.radius_pt * 2, points_per_pixel);
+            //log("player {},{} size={}", .{px.x, px.y, size});
+            w4.oval(px.x, px.y, @intCast(size), @intCast(size));
+        }
+
+        // draw the direction dot
+        {
+            const radius_pt: f32 = @as(f32, @floatFromInt(blob.radius_pt));
+            const px = ptToPx(points_per_pixel, .{
+                .x = blob.pos_pt.x + @as(i32, @intFromFloat(radius_pt * cosines[i])),
+                .y = blob.pos_pt.y + @as(i32, @intFromFloat(radius_pt * sines[i])),
+            });
+            w4.DRAW_COLORS.* = 0x41;
+            w4.oval(px.x - 2, px.y - 2, 4, 4);
+        }
     }
     // draw arena border
     {
@@ -248,8 +318,8 @@ export fn update() void {
             &buf,
             "{},{}",
             .{
-                @divTrunc(global.pos.x, 100),
-                @divTrunc(global.pos.y, 100),
+                @divTrunc(global.blobs[0].pos_pt.x, 100),
+                @divTrunc(global.blobs[0].pos_pt.y, 100),
             },
         ) catch @panic("codebug");
         w4.DRAW_COLORS.* = 0x04;
